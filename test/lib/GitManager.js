09f144dad8b39612e894434f53c210c2e1547e83
James Marks <marks@vgkids.com>
2023-08-10 18:48:03 -0700

    WIP push dir to github

--- test/lib/GitManager.js
+++ test/lib/GitManager.js
@@ -10,9 +10,16 @@ describe("GitManager", () => {
 
   describe('initializes repo', () => {
 
-    let github_repo = 'https://github.com/solid-adventure/slug-example.git'
-    let local_path = 'support/slug-example'
-    let gitManager = new GitManager(github_repo, local_path)
+    // let github_repo = 'https://github.com/solid-adventure/slug-example.git'
+
+    let org='solid-adventure'
+    let repo='slug-example'
+    let branch='main'
+    let local_path = 'test/support/slug-example/'
+    let gitManager = new GitManager(org, repo, branch, local_path)
+    gitManager.pushDirectory()
+
+
 
   })
 
